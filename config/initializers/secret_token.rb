c74a5f92bfd86810afbce733209bd2e3028cd8ec
Jenelle <jenellegab@gmail.com>
2014-05-23 18:10:51 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spiel::Application.config.secret_token = '1f914d72d2e4bc64b9f2f32f6bf91c2fe9c9823640709b4c2ec0e5f8d980968e43f90df8c5b477ecf33f766ac832d17b0e562643176d3af7cbb5bdd2e250cbfc'
